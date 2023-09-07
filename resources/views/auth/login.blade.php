c7547b64ac35f6039a7e2466e0473944b8727560
Greenhills Peligro <peligrogreenhills@gmail.com>
2023-09-08 06:45:45 +0800

    Modified login interface.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +69,143 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
+<body class="login-page">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="{{ route ('todos.index') }}">
+                To-Do App
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav me-auto">
+
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ms-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                        @endif
+
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="login-header box-shadow">
+        <div class="container-fluid d-flex justify-content-between align-items-center">
+            <div class="brand-logo">
+                <a href="login.html">
+                    <img src="/back/vendors/images/deskapp-logo.svg" alt="" />
+                </a>
+            </div>
+            <div class="login-menu">
+                <ul>
+                    <li><a href="register.html">Register</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="login-wrap d-flex align-items-center flex-wrap justify-content-center" >
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6 col-lg-7">
+                    <img src="/back/vendors/images/login-page-img.png" alt="" />
+                </div>
+                <div class="col-md-6 col-lg-5">
+                    <div class="login-box bg-white box-shadow border-radius-10">
+                        <div class="login-title">
+                            <h2 class="text-center text-primary">Login To DeskApp</h2>
+                        </div>
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+
+                            <div class="input-group custom">
+                                <input type="text" class="form-control form-control-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email"/>
+                                <div class="input-group-append custom">
+                                    <span class="input-group-text"><i class="icon-copy dw dw-user1"></i></span>
+                                </div>
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                            <div class="input-group custom">
+                                <input type="password" class="form-control form-control-lg @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="**********"/>
+                                <div class="input-group-append custom">
+                                    <span class="input-group-text"><i class="dw dw-padlock1"></i></span>
+                                </div>
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="row pb-30">
+                                <div class="col-6">
+                                    <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" class="custom-control-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}/>
+                                        <label class="custom-control-label" for="remember">Remember</label>
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="forgot-password">
+                                        <a href="forgot-password.html">Forgot Password</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="input-group mb-0">
+                                        <button type="submit" class="btn btn-primary" href="index.html">Sign In</button>
+                                    </div>
+                                    <div class="font-16 weight-600 pt-10 pb-10 text-center" data-color="#707373">
+                                        OR
+                                    </div>
+                                    <div class="input-group mb-0">
+                                        <a class="btn btn-outline-primary btn-lg btn-block" href="register.html">Register To Create Account</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,11 +10,22 @@
     <title>{{ config('app.name', 'To-do App') }}</title>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    {{-- <link rel="dns-prefetch" href="//fonts.bunny.net"> --}}
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet"/>
+
+    {{-- <!-- Site favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/back/vendors/images/apple-touch-icon.png"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="/back/vendors/images/favicon-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="/back/vendors/images/favicon-16x16.png"/> --}}
 
     <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="/back/vendors/styles/core.css" />
+    <link rel="stylesheet" type="text/css" href="/back/vendors/styles/icon-font.min.css"/>
+    <link rel="stylesheet" type="text/css" href="/back/src/plugins/datatables/css/dataTables.bootstrap4.min.css"/>
+    <link rel="stylesheet" type="text/css" href="/back/src/plugins/datatables/css/responsive.bootstrap4.min.css"/>
+    <link rel="stylesheet" type="text/css" href="/back/vendors/styles/style.css" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ url('css/styles.css')}}">
     @yield('styles')
     
     <!-- Scripts -->
@@ -80,5 +91,17 @@
             @yield('content')
         </main>
     </div>
+
+		<!-- js -->
+		<script src="/back/vendors/scripts/core.js"></script>
+		<script src="/back/vendors/scripts/script.min.js"></script>
+		<script src="/back/vendors/scripts/process.js"></script>
+		<script src="/back/vendors/scripts/layout-settings.js"></script>
+		<script src="/back/src/plugins/apexcharts/apexcharts.min.js"></script>
+		<script src="/back/src/plugins/datatables/js/jquery.dataTables.min.js"></script>
+		<script src="/back/src/plugins/datatables/js/dataTables.bootstrap4.min.js"></script>
+		<script src="/back/src/plugins/datatables/js/dataTables.responsive.min.js"></script>
+		<script src="/back/src/plugins/datatables/js/responsive.bootstrap4.min.js"></script>
+		<script src="/back/vendors/scripts/dashboard3.js"></script>
 </body>
 </html>
